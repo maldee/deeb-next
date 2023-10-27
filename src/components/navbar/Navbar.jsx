e03f0b617784e9d4cd3e36de3e97f0ab884fc80c
unknown <maliciousgemba@gmail.com>
2023-10-27 11:51:55 +0530

    ux2

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,16 +4,24 @@ import Image from "next/image";
 import Link from "next/link";
 import AuthLinks from "../authLinks/AuthLinks";
 import ThemeToggle from "../themeToggle/ThemeToggle";
+import { FaSearch } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.logo}>
         <a href="/" className={styles.logoLink}>
-        <Image src="/logo.png" alt="deeflow" width={40} height={40} />
+          <Image src="/logo.png" alt="deeflow" width={40} height={40} />
         </a>
+        <div className={styles.searchIcon}>
+          <FaSearch />
+        </div>
       </div>
-  
+
+
+
+
+
       <div className={styles.links}>
         <ThemeToggle />
         <AuthLinks />
