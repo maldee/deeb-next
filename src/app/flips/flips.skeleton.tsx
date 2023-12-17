6f1f48da00a454131c3496dfec5cbe717ffb1408
unknown <maliciousgemba@gmail.com>
2023-12-17 23:21:14 +0530

    newhobby79

--- src/app/flips/flips.skeleton.tsx
+++ src/app/flips/flips.skeleton.tsx
@@ -6,11 +6,13 @@ const FlipsSkeleton = () => {
     return (
         <SkeletonTheme baseColor="#1f273a" highlightColor="#0F1729">
         <ul className="list">
-        {Array(7)
+        {Array(3)
             .fill(1, 2)
             .map((item, index) => (
                 
         <div key={item.id} className={styles.skeletonBody}>
+             <Skeleton  className={styles.skeletonItem} />
+             <Skeleton  className={styles.skeletonItem} />
              <Skeleton  className={styles.skeletonItem} />
              <Skeleton  className={styles.skeletonItem} />
              <Skeleton  className={styles.skeletonItem} />
