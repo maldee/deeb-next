296fb1798c099beaf7d562d3a160485e11551bef
unknown <maliciousgemba@gmail.com>
2024-01-13 05:58:19 +0530

    newhobby130

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,23 +1,16 @@
-'use client';
-
 import styles from "./blog.module.css";
 import Featured from "../../components/featured/Featured";
 import CardList from "../../components/cardList/CardList";
 import Menu from "../../components/Menu/Menu";
 import SlideMenu from "../../components/slideMenu/slideMenu";
-import { GoogleAnalytics } from '@next/third-parties/google'
-
-export const metadata = {
-  title: "Blog | Deeflow",
-  description: "Discover new knowledge with deeflow",
-};
+import { GoogleTagManager  } from '@next/third-parties/google'
 
 export default function Blog({ searchParams }) {
   const page = parseInt(searchParams.page) || 1;
 
   return (
     <div className={styles.container}>
-      <GoogleAnalytics gaId="G-LFHZ053M0Z" />
+      <GoogleTagManager gaId="G-LFHZ053M0Z" />
       <div className={styles.postList}>
         <SlideMenu />
         <div className={styles.postGrid}>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,3 @@
-'use client';
-
 import styles from "./homepage.module.css";
 import Hero from "../components/home-hero/Hero";
 import Analytics from "../components/home-analytics/Analytics";
@@ -8,7 +6,7 @@ import Testimonial from "../components/home-testimonial/Testimonial";
 import Mentors from "../components/home-mentors/Mentors";
 import Subscribe from "../components/home-subscribe/Subscribe";
 import Flows from "../components/home-flows/Flows";
-import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 export default function Home({ searchParams }) {
 
@@ -17,7 +15,6 @@ export default function Home({ searchParams }) {
     return (
 
         <div className={styles.container}>
-            <GoogleAnalytics gaId="G-LFHZ053M0Z" />
             <Hero />
             <Analytics />
             <Flows />
