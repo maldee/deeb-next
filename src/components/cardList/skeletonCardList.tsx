bfe6f13ee739f845318139f6265b14b2c9347f9f
unknown <maliciousgemba@gmail.com>
2024-01-26 10:25:50 +0530

    newhobby164

--- src/components/cardList/skeletonCardList.tsx
+++ src/components/cardList/skeletonCardList.tsx
@@ -6,19 +6,26 @@ const SkeletonCardList = () => {
     return (
         <SkeletonTheme baseColor="#1f273a" highlightColor="#0F1729">
         <ul className="list">
-        {Array(3)
+        {Array(5)
             .fill(1, 2)
             .map((item, index) => (
-                <div key={item.id} className={styles.skeletonBody}>
-                <Skeleton  className={styles.skeletonItem} />
-                <Skeleton  className={styles.skeletonItem} />
-                <Skeleton  className={styles.skeletonItem} />
-                <Skeleton  className={styles.skeletonItem} />
-                <Skeleton  className={styles.skeletonItem} />
-                  
-               </div>
-        
-           ))}
+        <div key={item.id} className={styles.skeletonCardContainer}>
+            <div className={styles.skeletonCardImage} >
+                
+                    <Skeleton className={styles.skeletonCard} />
+                
+            </div>
+            <div className={styles.skeletonCardBody} >
+                
+                    <Skeleton className={styles.skeletonCardBodyDetails} height={50} width={350} />
+                    <Skeleton className={styles.skeletonCardBodyDetails} height={20} width={180} />
+                    <Skeleton className={styles.skeletonCardBodyDetails} height={30} width={300} />
+                    <Skeleton className={styles.skeletonCardBodyDetails} height={20} width={120} />
+                
+            </div>
+            
+        </div>
+        ))}
           </ul>
           </SkeletonTheme>
     );
