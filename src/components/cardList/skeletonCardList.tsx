3908f918c1520824fb87e7cc6a8fa0136d41589c
unknown <maliciousgemba@gmail.com>
2024-01-26 10:57:06 +0530

    newhobby167

--- src/components/cardList/skeletonCardList.tsx
+++ src/components/cardList/skeletonCardList.tsx
@@ -5,29 +5,32 @@ import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 const SkeletonCardList = () => {
     return (
         <SkeletonTheme baseColor="#1f273a" highlightColor="#0F1729">
-        <ul className="list">
-        {Array(5)
-            .fill(1, 2)
-            .map((item, index) => (
-        <div key={item.id} className={styles.skeletonCardContainer}>
-            <div className={styles.skeletonCardImage} >
-                
-                    <Skeleton className={styles.skeletonCard} />
-                
-            </div>
-            <div className={styles.skeletonCardBody} >
-                
-                    <Skeleton className={styles.skeletonCardBodyDetails} height={50} width={350} />
-                    <Skeleton className={styles.skeletonCardBodyDetails} height={20} width={180} />
-                    <Skeleton className={styles.skeletonCardBodyDetails} height={30} width={300} />
-                    <Skeleton className={styles.skeletonCardBodyDetails} height={20} width={120} />
-                
-            </div>
-            
-        </div>
-        ))}
-          </ul>
-          </SkeletonTheme>
+            <ul className="list">
+                {Array(5)
+                    .fill(1, 2)
+                    .map((item, index) => (
+                        <div key={item.id} className={styles.skeletonBody}>
+                            <Skeleton className={styles.skeletonItem} />
+                            <div key={item.id} className={styles.skeletonCardContainer}>
+                                <div className={styles.skeletonCardImage} >
+
+                                    <Skeleton className={styles.skeletonCard} />
+
+                                </div>
+                                <div className={styles.skeletonCardBody} >
+
+                                    <Skeleton className={styles.skeletonCardBodyDetails} height={50} width={350} />
+                                    <Skeleton className={styles.skeletonCardBodyDetails} height={20} width={180} />
+                                    <Skeleton className={styles.skeletonCardBodyDetails} height={30} width={300} />
+                                    <Skeleton className={styles.skeletonCardBodyDetails} height={20} width={120} />
+
+                                </div>
+
+                            </div>
+                        </div>
+                    ))}
+            </ul>
+        </SkeletonTheme>
     );
 };
 export default SkeletonCardList;
