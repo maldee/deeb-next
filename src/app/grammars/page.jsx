c99066040ffbe36c8518f49054d85af22e96c7b1
unknown <maliciousgemba@gmail.com>
2024-03-17 02:01:01 +0530

    newhobby258

--- src/app/grammars/page.jsx
+++ src/app/grammars/page.jsx
@@ -384,7 +384,7 @@ const Grammars = ({ searchParams }) => {
 
                   {item.note != 'None' ?
                     <div>
-                      <p className={styles.diff}>{'Notes ❕'}</p>
+                      <p className={styles.diff}>{'Notes 🛈'}</p>
                       <p className={styles.diffText}>{item.note}</p>
                     </div>
                     : null
