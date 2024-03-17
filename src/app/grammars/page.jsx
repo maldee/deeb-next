5bedac504bc6ca5cc5c39195638816a3a8b5b752
unknown <maliciousgemba@gmail.com>
2024-03-17 23:50:51 +0530

    newhobby261

--- src/app/grammars/page.jsx
+++ src/app/grammars/page.jsx
@@ -134,6 +134,16 @@ const Grammars = ({ searchParams }) => {
     setCurrentPage(1)
   }
 
+  function handleTag(e){
+    setQuery(e.target.value)
+    setUsage('Select Usage')
+    setFormality("Select Formality")
+    setType("select Type")
+    setTense("Select Tense")
+    setPlacement("Select Placement")
+    setCurrentPage(1)
+  }
+
   return (
     <div className={styles.container}>
       <GoogleTagManager gtmId="G-LFHZ053M0Z" />
@@ -248,7 +258,14 @@ const Grammars = ({ searchParams }) => {
               ))}
 
           </select>
-
+          <div className={styles.emojFilterMobile}>
+          Search by
+          <ul className={styles.filterEmojList}>
+          <button className={styles.emojBtn} onClick={handleTag} value={`🆚`}>🆚</button>
+          <button className={styles.emojBtn} onClick={handleTag} value={`*️⃣`}>*️⃣</button>
+          
+          </ul>
+        </div>
         </section>
       </div>
 
@@ -359,44 +376,57 @@ const Grammars = ({ searchParams }) => {
           onPageChange={setCurrentPage}
         />
       ) : (null)}
-
-      <div className={styles.phraseList}>
-        {isLoading
-          ? <ChatbitsSkeleton count={5} />
-          : data?.phrases.length > 0 ? (
-            data?.phrases?.map((item) => (
-              <div key={item.id} className={styles.container}>
-                <ul className={styles.chatCard}>
-                  <h3 key={item.id}>{item.phrase}</h3>
-                  <h4 className={styles.engp}>{item.sin_p}</h4>
-                  <hr className={styles.horiLine} />
-                  <h4 className={styles.example}>{item.example.split('-')[0]}</h4>
-                  <h4 className={styles.example}>{item.example.split('-')[1]}</h4>
-                  <br/>
-                  <div className={styles.details}>
-                    <p className={styles.tag}><span className={styles.formality}>Formality: </span> {item.formality}  </p>
-                    <p className={styles.tag}><span className={styles.tense}>Tense: </span> {item.tense}  </p>
-                    <p className={styles.tag}><span className={styles.type}>Type:</span> {item.type} </p>
-                    <p className={styles.tag}><span className={styles.placement}>Placement: </span> {item.placement}  </p>
-                    <p className={styles.tag}><span className={styles.usage}>Usage: </span> {item.usage} </p>
-                    <p className={styles.tag}><span className={styles.language}>Language: </span> {item.language}</p>
-                  </div>
-
-                  {item.note != 'None' ?
-                    <div>
-                      <p className={styles.diff}>{'Learn more'}</p>
-                      <p className={styles.diffText}>{item.note}</p>
+      <div className={styles.gridList}>
+        <div className={styles.phraseList}>
+          {isLoading
+            ? <ChatbitsSkeleton count={5} />
+            : data?.phrases.length > 0 ? (
+              data?.phrases?.map((item) => (
+                <div key={item.id} className={styles.container}>
+                  <ul className={styles.chatCard}>
+                    <h3 key={item.id}>{item.phrase}</h3>
+                    <h4 className={styles.engp}>{item.sin_p}</h4>
+                    <hr className={styles.horiLine} />
+                    <h4 className={styles.example}>{item.example.split('-')[0]}</h4>
+                    <h4 className={styles.example}>{item.example.split('-')[1]}</h4>
+                    <br />
+                    <div className={styles.details}>
+                      <p className={styles.tag}><span className={styles.formality}>Formality: </span> {item.formality}  </p>
+                      <p className={styles.tag}><span className={styles.tense}>Tense: </span> {item.tense}  </p>
+                      <p className={styles.tag}><span className={styles.type}>Type:</span> {item.type} </p>
+                      <p className={styles.tag}><span className={styles.placement}>Placement: </span> {item.placement}  </p>
+                      <p className={styles.tag}><span className={styles.usage}>Usage: </span> {item.usage} </p>
+                      <p className={styles.tag}><span className={styles.language}>Language: </span> {item.language}</p>
                     </div>
-                    : null
-                  }
-                </ul>
-              </div>
 
-            ))) : (
-              
-            <h3 className={styles.noResults}> 🙄 No results. Try different selection</h3>
-          )}
+                    {item.note != 'None' ?
+                      <div>
+                        <p className={styles.diff}>{'Learn more'}</p>
+                        <p className={styles.diffText}>{item.note}</p>
+                      </div>
+                      : null
+                    }
+                  </ul>
+                </div>
+
+              ))) : (
+
+              <h3 className={styles.noResults}> 🙄 No results. Try different selection</h3>
+            )}
+
+
+        </div>
+        <div className={styles.emojTagList}>
+          Tags
+          <ul className={styles.tagEmojList}>
+          <button className={styles.emojBtn} onClick={handleTag} value={`🆚`}>🆚</button>
+          <button className={styles.emojBtn} onClick={handleTag} value={`*️⃣`}>*️⃣</button>
+         
+          </ul>
+        </div>
       </div>
+     
+
     </div>
   )
 }
