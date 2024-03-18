8f039e4d4df8ea1b4f21bdf584ea19c2f6c02b42
unknown <maliciousgemba@gmail.com>
2024-03-18 12:19:37 +0530

    newhobby266

--- src/app/plans/page.jsx
+++ src/app/plans/page.jsx
@@ -15,31 +15,39 @@ export default function About({ searchParams }) {
       <div className={styles.logoContainer}>
         <img className={styles.logo} src="./deeflow_logo.jpg" />
       </div>
-      <br />
+
       <div className={styles.container}>
         <p className={styles.aboutDesc} >Upgrade your plan to Pro</p>
         <br />
         <h1 className={styles.subTitle}>Pro Plan Required</h1>
-        <div className={styles.planDetails}>
-          <img className={styles.plan_logo} src="./pro_plan.jpg" />
-          <div className={styles.details}>
-            <p>✅ Access to 5000+ words dictionary </p>
-            <p>✅ infinite Quizes to enhance your result </p>
-            <p>✅ Exam Cheatsheets to find anwers quickly </p>
-            <p>✅ Detailed Grammar repository with examples</p>
-            <p>✅ Easy vocabulary tests for commit to your memory</p>
-            <p>✅ Question and answer platform to get support And many more</p>
+        <div className={styles.planContent}>
 
-<br/>
-            <Link href="/">
-              <button className={styles.upgradeButtonActivated} >{'Upgrade LKR 3000/yr'}</button>
-             
-            </Link>
-            <h3 className={styles.wish} >Happy exam ❤️</h3>
-          </div>
+            <div className={styles.planDetails}>
+              <img className={styles.plan_logo} src="./pro_plan.jpg" />
+
+            </div>
+
+            <div className={styles.details}>
+              <p>✅ Access to 5000+ words dictionary </p>
+              <p>✅ infinite Quizes to enhance your result </p>
+              <p>✅ Exam Cheatsheets to find anwers quickly </p>
+              <p>✅ Detailed Grammar repository with examples</p>
+              <p>✅ Easy vocabulary tests for commit to your memory</p>
+              <p>✅ Question and answer platform to get support And many more</p>
+
+              <br />
+              <div className={styles.upgradeBtn}>
+                <Link href="/">
+                  <button className={styles.upgradeButtonActivated} >{'Upgrade LKR 3000/yr'}</button>
+
+                </Link>
+                <h3 className={styles.wish} >Happy exam ❤️</h3>
+              </div>
+
+            </div>
+          
         </div>
 
-        
       </div>
     </div>
   );
