5768df1c92ac00e948411df0fa4678a6a96807c7
unknown <maliciousgemba@gmail.com>
2024-03-26 21:07:00 +0530

    newhobby328

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -12,9 +12,6 @@ const Menu = () => {
       <h2 className={styles.subtitle}>{"What's hot"}</h2>
       <h1 className={styles.title}>Most Popular</h1>
       <MenuPosts withImage={false} />
-     
-   
-     
     </div>
   );
 };
